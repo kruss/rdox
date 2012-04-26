4164c6359673ee3088260696f89a4a05ca632998
Kevin Russ <kruss@muc-20-1126.(none)>
2012-04-26 16:16:25 +0200

    wip: map

--- lib/rdox/generated.rb
+++ lib/rdox/generated.rb
@@ -1,6 +1,6 @@
 $GEM = "rdox"
 $VERSION = "0.1.0"
-$DATE = "2012-04-26 15:53:44"
+$DATE = "2012-04-26 16:00:21"
 $HELP = <<END_OF_MESSAGE
 rdox is a gem for rake based documents
 
--- lib/rdox/tasks/build_task.rb
+++ lib/rdox/tasks/build_task.rb
@@ -34,43 +34,6 @@ private
 			build_content(child)
 		end
 	end
-	
-	def build_map(element)
-		target = "#{$OUTPUT}/#{element.id}-map.html"
-		puts "build: #{target}"
-		File.open(target, 'w') { |output| 
-			output.write("<html><title>#{element.name} - Map</title><head>\r\n") 
-			output.write("<!-- #{$GEM} (#{$VERSION}) build #{$DATE} //-->\r\n")
-			output.write(get_css)
-			output.write("</head><body>\r\n")
-			output.write("<hr>&lt; <a href='#{element.id}.html'>#{element.name}</a><hr>\r\n")
-			output.write("<h1>#{element.name} - Map</h1>\r\n") 
-			output.write("<hr><p>\r\n")
-			if element.childs? then
-				write_map(output, element)
-			else
-				output.write("<i>empty</i>\r\n")
-			end
-			output.write("</p><hr>#{$GEM} (#{$VERSION})<hr>\r\n")
-			output.write("</body></html>\r\n") 
-		}
-	end
-	
-	def write_map(output, element)
-		output.write("<ol>\r\n")
-		element.childs.each do |child|
-			output.write("<li>")
-			output.write("<b><a href='#{child.id}.html'>#{child.name}</a></b>\r\n")
-			if child.description != nil then
-				output.write(" - #{child.description}\r\n")
-			end
-			output.write("</li>\r\n")
-			if child.childs? then
-				write_map(output, child)
-			end
-		end
-		output.write("</ol>\r\n")
-	end
 		
 	def write_content_header(output, element)
 		output.write("<html><title>#{element.name}</title><head>\r\n") 
@@ -121,6 +84,43 @@ private
 		output.write("<td align=right>#{date.strftime("%Y-%m-%d %H:%M:%S")}</td></tr>\r\n")
 		output.write("</table><hr>\r\n")
 		output.write("</body></html>\r\n") 
+	end
+	
+	def build_map(element)
+		target = "#{$OUTPUT}/#{element.id}-map.html"
+		puts "build: #{target}"
+		File.open(target, 'w') { |output| 
+			output.write("<html><title>#{element.name} - Map</title><head>\r\n") 
+			output.write("<!-- #{$GEM} (#{$VERSION}) build #{$DATE} //-->\r\n")
+			output.write(get_css)
+			output.write("</head><body>\r\n")
+			output.write("<hr>&lt; <a href='#{element.id}.html'>#{element.name}</a><hr>\r\n")
+			output.write("<h1>#{element.name} - Map</h1>\r\n") 
+			output.write("<hr><p>\r\n")
+			if element.childs? then
+				write_map(output, element)
+			else
+				output.write("<i>empty</i>\r\n")
+			end
+			output.write("</p><hr>#{$GEM} (#{$VERSION})<hr>\r\n")
+			output.write("</body></html>\r\n") 
+		}
+	end
+	
+	def write_map(output, element)
+		output.write("<ol>\r\n")
+		element.childs.each do |child|
+			output.write("<li>")
+			output.write("<b><a href='#{child.id}.html'>#{child.name}</a></b>\r\n")
+			if child.description != nil then
+				output.write(" - #{child.description}\r\n")
+			end
+			output.write("</li>\r\n")
+			if child.childs? then
+				write_map(output, child)
+			end
+		end
+		output.write("</ol>\r\n")
 	end
 	
 	def get_css
